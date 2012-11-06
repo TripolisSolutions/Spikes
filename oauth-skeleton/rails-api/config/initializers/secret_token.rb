3a658e70090cbae3c5034e02b8d8470d43d62acb
Alexander Borovsky <alexander.borovsky@altoros.com>
2012-11-06 10:52:09 +0300

    Initial Rails-API-oauth application commit

---
+++ oauth-skeleton/rails-api/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApi::Application.config.secret_token = '0d139fae5f9e151b467817a4c258fb16c788a686613d1cfa90023699a2e08c5ded82c431192b6b9374a9d13d0b9a012b2fb91d267d00802dcf70661b80c99ac1'
