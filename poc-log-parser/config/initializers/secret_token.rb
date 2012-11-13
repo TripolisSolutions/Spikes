f96df21c2910889941624d388504969876d6a62c
Harald Walker <hwalker@tripolis.com>
2012-11-13 11:27:29 +0100

    Initial commit of new rails app

---
+++ poc-log-parser/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PocLogParser::Application.config.secret_token = 'dfb3b4a3503a00b16db5e9e25d87bdb6251c5f93212ebb2fea15b5d43432a0783e91da56a1fc189f023820335b5186d024753bb78918e72b1e303cd82f071296'
