e3da0d9e0481857386f76759a7c4d759a67a2821
Harald Walker <hwalker@tripolis.com>
2012-11-15 10:08:24 +0100

    #1013 Initial commit of new rails app for poc

---
+++ poc-page-meta-data-parser/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PocPageMetaDataParser::Application.config.secret_token = 'b19069ce554df91292923d69ff5b76adaebaf842d623a36b9347be271d5f0e68172fe96ca2282fb924bb59ccea3b08e8d995d270fe2ae0dcd10fbbd914bf79cb'
