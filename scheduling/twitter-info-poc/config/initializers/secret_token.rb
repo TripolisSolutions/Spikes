c5e3a84cb3dbae909104dd6ec721cc5f48f930be
Alexander Borovsky <alexander.borovsky@altoros.com>
2012-11-20 13:24:35 +0300

    Initial commit [#1006]

---
+++ scheduling/twitter-info-poc/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterInfoPoc::Application.config.secret_token = 'cee6757e9dd10bd802d26be8b676a9152872e77bd149376bb0d482ec836f5ed96ef0927a6fb81789129848f2025edab998247929ad72762035d49286464fee14'
