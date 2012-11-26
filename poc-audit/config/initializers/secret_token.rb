d4a02a758be6b055e5aa9135b46a5fe2709950b9
Alexander Borovsky <alexander.borovsky@altoros.com>
2012-11-26 15:16:58 +0300

    Added base POC code [#1217]

---
+++ poc-audit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PocAudit::Application.config.secret_token = '8315f7d2bf253bb8f0d1184d4984abd888aedc6858a9c4a6f546e985dab6e09753a869dc6698bb669b1f20a1ba29b09f78955c549a9e67395226b669479b25f9'
