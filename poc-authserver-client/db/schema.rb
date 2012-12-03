e4e64413d53df5019bea12fc5d14deecd3973ea0
Alexander Borovsky <alexander.borovsky@altoros.com>
2012-12-03 14:50:41 +0300

    Fixed tests

--- poc-authserver-client/db/schema.rb
+++ poc-authserver-client/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121107150048) do
+ActiveRecord::Schema.define(:version => 20121108220554) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
@@ -21,26 +21,16 @@ ActiveRecord::Schema.define(:version => 20121107150048) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "users", :force => true do |t|
-    t.datetime "created_at",                                            :null => false
-    t.datetime "updated_at",                                            :null => false
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",                    :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.integer  "oauth2_uid",             :limit => 8
-    t.string   "oauth2_token",           :limit => 149
-    t.text     "name"
+  create_table "stuffs", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["oauth2_uid"], :name => "index_users_on_oauth2_uid", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
