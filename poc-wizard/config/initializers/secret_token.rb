55759f83e3de16564a42da2e7d82156562c6bdf1
jeroen77 <jrondeel@tripolis.com>
2013-11-19 10:44:54 +0100

    Add poc-wizard project

---
+++ poc-wizard/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wizard::Application.config.secret_key_base = 'd98e77083b7bcacdbe4ed3d03c90fed52f48ae2db99db2b3c9099705ce9192055dc542347f50f0b32ed728127c2bcc03ed8e435c81789d38725c32d889b80cdc'
